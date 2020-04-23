24b91bb20c818db1e9ce221d4ed9f5d16576203c
GutoCaneppele <augusto.caneppele@gmail.com>
2020-04-23 23:01:47 +0200

    first commit

---
+++ 01_temperatura.py
@@ -0,0 +1,11 @@
+print('Bem vindo')
+
+# leitura e conversao
+temp_celsius = input('Informe a temperatura: ')
+print(type(temp_celsius))
+temp_celsius = float(temp_celsius)
+print(type(temp_celsius))
+
+# transformacao
+temp_farenheit = (temp_celsius * 1.8) + 32
+print('A temperatura ' + str(temp_celsius) + ' em celsius equivale a ' + str(temp_farenheit))
